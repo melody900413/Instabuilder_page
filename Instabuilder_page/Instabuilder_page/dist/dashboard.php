caa53abe1ccd99e6eb410f4acf7f7e4b57a0eabe
melody900413 <10556023@ntub.edu.tw>
2020-09-13 22:09:02 +0800

    Create
    home-->login&siign up-->dashboard

--- Instabuilder_page/Instabuilder_page/dist/dashboard.php
+++ Instabuilder_page/Instabuilder_page/dist/dashboard.php
@@ -21,16 +21,16 @@
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <a class="navbar-brand" href="index.html">Instabuilder</a>
+            <a class="navbar-brand" href="home.html">Instabuilder</a>
             <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-                <div class="input-group">
+                <!-- <div class="input-group">
                     <input class="form-control" type="text" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" />
                     <div class="input-group-append">
                         <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
                     </div>
-                </div>
+                </div> -->
             </form>
             <!-- Navbar-->
             <ul class="navbar-nav ml-auto ml-md-0">
@@ -55,9 +55,9 @@
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>-->
-                            <a class="nav-link" href="index.html">
+                            <a class="nav-link" href="home.html">
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                                關於我們
+                                關於我們&產品購買
                             </a>
                             <div class="sb-sidenav-menu-heading">Instabuilder 功能</div>
                             <!--<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
